0678a0a22f077db334a8a2b9ba41cab32976deb4
halil.olcay <holcay92@gmail.com>
2022-12-12 22:00:44 +0300

    ViewModelProvider.Factory override create function error is solved by removing nullable ? mark

--- app/src/main/java/com/example/roomdatabasedemo/StudentViewModelFactory.kt
+++ app/src/main/java/com/example/roomdatabasedemo/StudentViewModelFactory.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.roomdatabasedemo.db.StudentDao
 
 class StudentViewModelFactory(private val dao: StudentDao):ViewModelProvider.Factory{
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(StudentViewModel::class.java)){
             Log.d("StudentViewModelFactory", "create: ")
             return StudentViewModel(dao) as T
