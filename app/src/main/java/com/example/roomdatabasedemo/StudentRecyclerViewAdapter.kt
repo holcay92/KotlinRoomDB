80732770acdeab5015d31668f63c91ca749a29b5
halil.olcay <holcay92@gmail.com>
2022-12-13 00:09:00 +0300

    recyclerview class is finished

---
+++ app/src/main/java/com/example/roomdatabasedemo/StudentRecyclerViewAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.roomdatabasedemo
+
+import android.view.LayoutInflater
+import android.view.View.inflate
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomdatabasedemo.db.Student
+
+class StudentRecyclerViewAdapter() :RecyclerView.Adapter<StudentViewHolder>(){
+
+    private val studentList= arrayListOf<Student>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        //layout object is created
+       val layoutInflater = LayoutInflater.from(parent.context)
+        val listItem = layoutInflater.inflate(R.layout.list_item,parent,false)
+        return StudentViewHolder(listItem)
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+       return holder.bind(studentList[position])
+    }
+
+    override fun getItemCount(): Int {
+        //return the size of the list
+        return studentList.size
+    }
+
+    fun setList(list:List<Student>){
+        //thats because we want to update the list
+        studentList.clear()
+        studentList.addAll(list)
+    }
+
+}
+}
---
+++ app/src/main/java/com/example/roomdatabasedemo/StudentViewHolder.kt
@@ -0,0 +1,15 @@
+package com.example.roomdatabasedemo
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomdatabasedemo.db.Student
+
+class StudentViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    fun bind(student: Student) {
+        val name = view.findViewById<TextView>(R.id.tvName)
+        val email = view.findViewById<TextView>(R.id.tvEmail)
+        name.text = student.name
+        email.text = student.email
+    }
+}
